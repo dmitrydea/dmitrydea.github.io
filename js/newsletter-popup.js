7ebad0cda2528790124d041bb520d84eaf35b7ec
Guk Dmitry <gukdmitry.s@gmail.com>
2023-01-12 21:58:59 +0200

    added forms

--- js/newsletter-popup.js
+++ js/newsletter-popup.js
@@ -1,23 +1,16 @@
 jQuery(document).ready(function(){  
-  jQuery('.ct-newsletter-close, .ct-newsletter-hide span').click(function(){
+  jQuery('.go-connection').click(function(e){
+    e.preventDefault();
+    jQuery('#ct-newsletter-popup').fadeIn();
+  });
+  jQuery('.go-buy').click(function(e){
+    e.preventDefault();
+    jQuery('#ct-buy-popup').fadeIn();
+  });
+  jQuery('#ct-buy-popup .ct-newsletter-close, #ct-buy-popup .ct-newsletter-hide span').click(function(){
+    jQuery('#ct-buy-popup').fadeOut();
+  });
+  jQuery('#ct-newsletter-popup .ct-newsletter-close, #ct-newsletter-popup .ct-newsletter-hide span').click(function(){
     jQuery('#ct-newsletter-popup').fadeOut();
   });
-  
-  var visits = jQuery.cookie('visits') || 0;
-  visits++;
-  
-  jQuery.cookie('visits', visits, { expires: 1, path: '/' });
-    
-  console.debug(jQuery.cookie('visits'));
-    
-  if ( jQuery.cookie('visits') > 1 ) {
-    jQuery('#ct-newsletter-popup').hide();
-  } else {
-    var pageHeight = jQuery(document).height();
-    setTimeout(function(){
-      jQuery('#ct-newsletter-popup').fadeIn();
-    }, 5000);
-  }
-
-  if (jQuery.cookie('noShowWelcome')) { jQuery('#ct-newsletter-popup').hide(); }
 }); 
