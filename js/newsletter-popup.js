d8bf94f7e8a785f922358940b73905867f03958c
Guk Dmitry <gukdmitry.s@gmail.com>
2023-01-17 00:05:26 +0200

    added vaucher

--- js/newsletter-popup.js
+++ js/newsletter-popup.js
@@ -7,10 +7,17 @@ jQuery(document).ready(function(){
     e.preventDefault();
     jQuery('#ct-buy-popup').fadeIn();
   });
+  jQuery('.go-happy').click(function(e){
+    e.preventDefault();
+    jQuery('#ct-happy-popup').fadeIn();
+  });
   jQuery('#ct-buy-popup .ct-newsletter-close, #ct-buy-popup .ct-newsletter-hide span').click(function(){
     jQuery('#ct-buy-popup').fadeOut();
   });
   jQuery('#ct-newsletter-popup .ct-newsletter-close, #ct-newsletter-popup .ct-newsletter-hide span').click(function(){
     jQuery('#ct-newsletter-popup').fadeOut();
+  });
+  jQuery('#ct-happy-popup .ct-newsletter-close, #ct-happy-popup .ct-newsletter-hide span').click(function(){
+    jQuery('#ct-happy-popup').fadeOut();
   });
 }); 
